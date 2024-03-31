c64afec5cc8fcabd46734d72b99f0914ab1b697c
kammmmi122 <kammmmi122@gmail.com>
2024-03-31 18:38:24 +0200

    Update

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 asciidoctor -a docinfo=shared --backend=html5 ./*.adoc
-asciidoctor -a docinfo=shared --backend=html5 -D ./Przepisy/* ./Przepisy/*.adoc
+asciidoctor -a docinfo=shared --backend=html5 -D ./Przepisy ./Przepisy/*.adoc
 asciidoctor -a docinfo=shared --backend=html5 -D ./Przepisy/Przetwory ./Przepisy/Przetwory/*.adoc
 asciidoctor -a docinfo=shared --backend=html5 -D ./Przepisy/Desery ./Przepisy/Desery/*.adoc
 asciidoctor -a docinfo=shared --backend=html5 -D ./Przepisy/Dania_glowne ./Przepisy/Dania_glowne/*.adoc
