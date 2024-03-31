42810afe13afb99e690a807aad14f6de0f607658
kammmmi122 <kammmmi122@gmail.com>
2024-03-31 23:57:07 +0200

    Update

--- create_index.py
+++ create_index.py
@@ -3,14 +3,19 @@ import os
 
 def create_index_adoc():
     with open(f"index.adoc", "w", encoding="utf8") as file2:
-        pass
+        file2.write("= Lista przepisów\n\n")
+        file2.write("== Przepisy\n\n")
 
     for path, subdirs, files in os.walk("."):
         for name in files:
             if name.endswith("adoc") and name != "index.adoc":
-                path_to_html = os.path.join(path.replace(".\\", ""), name.replace("adoc", "html")).replace("\\", "/")
+                path_to_html = os.path.join(
+                    path.replace(".\\", ""), name.replace("adoc", "html")
+                ).replace("\\", "/")
                 with open(f"index.adoc", "a+", encoding="utf8") as file2:
-                    file2.write(f"1. link:{path_to_html}[{name.replace('_', ' ').capitalize()}]\n")
+                    file2.write(
+                        f"1. link:{path_to_html}[{name.replace('_', ' ').capitalize()}]\n"
+                    )
 
 
 if __name__ == "__main__":
