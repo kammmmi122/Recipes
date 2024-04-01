659c1ff1660a58a9117a72338ab3af04c56bc279
kammmmi122 <kammmmi122@gmail.com>
2024-04-01 12:02:41 +0200

    Add new folders

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 asciidoctor -a docinfo=shared --backend=html5 ./*.adoc
 asciidoctor -a docinfo=shared --backend=html5 -D ./Przepisy ./Przepisy/*.adoc
 asciidoctor -a docinfo=shared --backend=html5 -D ./Przepisy/Przetwory ./Przepisy/Przetwory/*.adoc
+asciidoctor -a docinfo=shared --backend=html5 -D ./Przepisy/Przystawki ./Przepisy/Przystawki/*.adoc
+asciidoctor -a docinfo=shared --backend=html5 -D ./Przepisy/Makarony ./Przepisy/Makarony/*.adoc
 asciidoctor -a docinfo=shared --backend=html5 -D ./Przepisy/Desery ./Przepisy/Desery/*.adoc
 asciidoctor -a docinfo=shared --backend=html5 -D ./Przepisy/Dania_glowne ./Przepisy/Dania_glowne/*.adoc
 asciidoctor -a docinfo=shared --backend=html5 -D ./Przepisy/Zupy ./Przepisy/Zupy/*.adoc
