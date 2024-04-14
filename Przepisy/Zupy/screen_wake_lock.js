1ed5abcd41dfbd3b475c94d5e5dc3e93c9e10dfc
kammmmi122 <kammmmi122@gmail.com>
2024-04-14 23:29:13 +0200

    Add screen wake lock

---
+++ Przepisy/Zupy/screen_wake_lock.js
@@ -0,0 +1,64 @@
+console.clear();
+
+// toggle button
+const wakeButton = document.querySelector('[data-status]');
+
+// change button and status if wakelock becomes acquired or is released
+const changeUI = (status = 'acquired') => {
+  const acquired = status === 'acquired' ? true : false;
+  wakeButton.dataset.status = acquired ? 'on' : 'off';
+  wakeButton.textContent = `Turn Wake Lock ${acquired ? 'OFF' : 'ON'}`;
+}
+
+// test support
+let isSupported = false;
+
+if ('wakeLock' in navigator) {
+  isSupported = true;
+} else {
+  wakeButton.disabled = true;
+}
+
+if (isSupported) {
+  // create a reference for the wake lock
+  let wakeLock = null;
+
+  // create an async function to request a wake lock
+  const requestWakeLock = async () => {
+    try {
+      wakeLock = await navigator.wakeLock.request('screen');
+
+      // change up our interface to reflect wake lock active
+      changeUI();
+
+      // listen for our release event
+      wakeLock.onrelease = function(ev) {
+        console.log(ev);
+      }
+      wakeLock.addEventListener('release', () => {
+        // if wake lock is released alter the button accordingly
+        changeUI('released');
+      });
+
+    } catch (err) {
+      // if wake lock request fails - usually system related, such as battery
+      wakeButton.dataset.status = 'off';
+      wakeButton.textContent = 'Turn Wake Lock ON';
+
+    }
+  } // requestWakeLock()
+
+  // if we click our button
+  wakeButton.addEventListener('click', () => {
+    // if wakelock is off request it
+    if (wakeButton.dataset.status === 'off') {
+      requestWakeLock()
+    } else { // if it's on release it
+      wakeLock.release()
+        .then(() => {
+          wakeLock = null;
+        })
+    }
+  })
+
+} // isSupported
