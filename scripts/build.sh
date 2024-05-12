62846ad325e279caba594e69afd5579c0efad8d2
kammmmi122 <kammmmi122@gmail.com>
2024-05-12 09:55:54 +0200

    Update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 asciidoctor -a docinfo=shared --backend=html5 ./*.adoc
-asciidoctor -a docinfo=shared --backend=html5 -D ./Przepisy ./Przepisy/*.adoc
-asciidoctor -a docinfo=shared --backend=html5 -D ./Przepisy/Przetwory ./Przepisy/Przetwory/*.adoc
-asciidoctor -a docinfo=shared --backend=html5 -D ./Przepisy/Przystawki ./Przepisy/Przystawki/*.adoc
-asciidoctor -a docinfo=shared --backend=html5 -D ./Przepisy/Makarony ./Przepisy/Makarony/*.adoc
-asciidoctor -a docinfo=shared --backend=html5 -D ./Przepisy/Desery ./Przepisy/Desery/*.adoc
-asciidoctor -a docinfo=shared --backend=html5 -D ./Przepisy/Dania_glowne ./Przepisy/Dania_glowne/*.adoc
-asciidoctor -a docinfo=shared --backend=html5 -D ./Przepisy/Zupy ./Przepisy/Zupy/*.adoc
-asciidoctor -a docinfo=shared --backend=html5 -D ./Przepisy/Salatki ./Przepisy/Salatki/*.adoc
+asciidoctor -a docinfo=shared --backend=html5 -D /Przepisy /Przepisy/*.adoc
+asciidoctor -a docinfo=shared --backend=html5 -D /Przepisy/Przetwory /Przepisy/Przetwory/*.adoc
+asciidoctor -a docinfo=shared --backend=html5 -D /Przepisy/Przystawki /Przepisy/Przystawki/*.adoc
+asciidoctor -a docinfo=shared --backend=html5 -D /Przepisy/Makarony /Przepisy/Makarony/*.adoc
+asciidoctor -a docinfo=shared --backend=html5 -D /Przepisy/Desery /Przepisy/Desery/*.adoc
+asciidoctor -a docinfo=shared --backend=html5 -D /Przepisy/Dania_glowne /Przepisy/Dania_glowne/*.adoc
+asciidoctor -a docinfo=shared --backend=html5 -D /Przepisy/Zupy /Przepisy/Zupy/*.adoc
+asciidoctor -a docinfo=shared --backend=html5 -D /Przepisy/Salatki /Przepisy/Salatki/*.adoc
