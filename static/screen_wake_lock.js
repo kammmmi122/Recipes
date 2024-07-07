f3b69a2427d8ed5405b2430746b87ab4c47b96d4
kammmmi122 <kammmmi122@gmail.com>
2024-07-07 18:24:19 +0200

    Update megalinter pre install (#78)

--- static/screen_wake_lock.js
+++ static/screen_wake_lock.js
@@ -1,16 +1,16 @@
 // toggle button
-const wakeButton = document.querySelector('[data-status]');
+const wakeButton = document.querySelector("[data-status]");
 
 // change button and status if wakelock becomes acquired or is released
-const changeUI = (status = 'acquired') => {
-  const acquired = status === 'acquired' ? true : false;
-  wakeButton.dataset.status = acquired ? 'on' : 'off';
-}
+const changeUI = (status = "acquired") => {
+  const acquired = status === "acquired" ? true : false;
+  wakeButton.dataset.status = acquired ? "on" : "off";
+};
 
 // test support
 let isSupported = false;
 
-if ('wakeLock' in navigator) {
+if ("wakeLock" in navigator) {
   isSupported = true;
 } else {
   wakeButton.disabled = true;
@@ -23,38 +23,35 @@ if (isSupported) {
   // create an async function to request a wake lock
   const requestWakeLock = async () => {
     try {
-      wakeLock = await navigator.wakeLock.request('screen');
+      wakeLock = await navigator.wakeLock.request("screen");
 
       // change up our interface to reflect wake lock active
       changeUI();
 
       // listen for our release event
-      wakeLock.onrelease = function(ev) {
+      wakeLock.onrelease = function (ev) {
         console.log(ev);
-      }
-      wakeLock.addEventListener('release', () => {
+      };
+      wakeLock.addEventListener("release", () => {
         // if wake lock is released alter the button accordingly
-        changeUI('released');
+        changeUI("released");
       });
-
     } catch (err) {
       // if wake lock request fails - usually system related, such as battery
-      wakeButton.dataset.status = 'off';
-
+      wakeButton.dataset.status = "off";
     }
-  } // requestWakeLock()
+  }; // requestWakeLock()
 
   // if we click our button
-  wakeButton.addEventListener('click', () => {
+  wakeButton.addEventListener("click", () => {
     // if wakelock is off request it
-    if (wakeButton.dataset.status === 'off') { 
-      requestWakeLock()
-    } else { // if it's on release it
-      wakeLock.release()
-        .then(() => {
-          wakeLock = null;
-        })
+    if (wakeButton.dataset.status === "off") {
+      requestWakeLock();
+    } else {
+      // if it's on release it
+      wakeLock.release().then(() => {
+        wakeLock = null;
+      });
     }
-  })
-
+  });
 } // isSupported
