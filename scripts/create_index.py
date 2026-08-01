295dbefd94468e926a9b4f2e74a2c408191cf1a8
kammmmi122 <kammmmi122@gmail.com>
2026-08-01 02:27:48 +0200

    Update

--- scripts/create_index.py
+++ scripts/create_index.py
@@ -150,7 +150,7 @@ def create_index_adoc():
 
                 # CARD HTML
                 card_html = (
-                    f'<article class="card" data-category="{html.escape(folder_name)}" data-rating="{rating}">'
+                    f'<article class="card" data-category="{html.escape(folder_name)}" data-rating="{rating}" data-emoji="{html.escape(emoji_html)}">'
                     f'<a class="card-main-link" href="{html.escape(path_to_html, quote=True)}">'
                     f"{category_label}"
                     f"{image_html}"
